a2ba9706a1d26c5565d06bbe6c3fe427a39f2edb
Fatih Turker <fthturker80@gmail.com>
2023-06-04 12:19:20 +0300

    04.06.23

--- C20_Break_And_Continue/BreakAndContinue.py
+++ C20_Break_And_Continue/BreakAndContinue.py
@@ -19,5 +19,35 @@ for i in liste:
 
 # kullanıcı bize ismini soylesin bu ismi bastıralım.
 # Ta ki kullanıcı "q" ya bastıgında dursun.
-while True:
-    isim=input("İsim: (Çıkmak için 'q' ya basın):" )
+# while True:
+#     isim=input("Adınızı Giriniz:")
+#     print("Çıkmak için 'q' ya basın")
+#     if(isim=="q"):
+#         print("Programdan çıkılıyor...")
+#         break
+#     print("İsminiz:",isim)
+
+#continue--> Döngü herhangi bir yerde ve herhangi bir zamanda continue ifadesiyle karşılaştığı zaman
+             #geri kalan işlemlerini yapmadan direk bloğunun başına döner.
+
+# liste = [1,2,3,4,5,6,7,8,9,10]
+# for i in liste:
+#     if(i==3 or i==5):
+#         continue # 3 v3 5 degerlerini çıktıda göremiyouz
+#     print("i:",i)
+
+i = 0
+
+while (i < 10):
+
+    if (i == 2):
+        i += 1 # Artırma işlemi
+        continue
+
+    print("i:",i)
+    i += 1
+
+
+
+
+
